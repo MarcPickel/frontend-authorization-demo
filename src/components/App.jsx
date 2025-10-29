aa3dad18490190372ab835cc1a807fddf1e5bcf5
MarcPickel <marcpc@att.net>
2025-10-29 18:28:30 -0500

    Add api.js and token.js with helpers for better UX and routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
 import * as auth from "../utils/auth";
+import * as api from "../utils/api";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
@@ -8,9 +15,30 @@ import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
 
+import { setToken, getToken } from "../utils/token";
+
 function App() {
   const [userData, setUserData] = useState({ username: "", email: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const jwt = getToken();
+
+    if (!jwt) {
+      return;
+    }
+
+    api
+      .getUserInfo(jwt)
+      .then(({ username, email }) => {
+        // If the response is successful, log the user in, save his
+        // data to state, and navigate him to /ducks.
+        setIsLoggedIn(true);
+        setUserData({ username, email });
+        navigate("/ducks");
+      })
+      .catch(console.error);
+  }, []);
 
   const navigate = useNavigate();
 
@@ -39,6 +67,7 @@ function App() {
       .authorize(username, password)
       .then((data) => {
         if (data.jwt) {
+          setToken(data.jwt);
           setUserData(data.user);
           setIsLoggedIn(true);
           navigate("/ducks");
